bb77c09174aafdc8eb0c43ef63bae13f69c8c73e
Israel Rodriguez Gallego <israelrodriguez@protonmail.com>
2020-06-24 00:03:32 +0200

    Archivo original

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const {app, BrowserWindow, Menu} = require ('electron');
+const url  = require('url'); //Pasar archivo interno
+const path = require('path');
+
+
+//Hacer el reload solo en sistema de produccion
+if (process.env.NODE_ENV !== 'production'){
+require('electron-reload')(__dirname, {
+})}
+
+let mainwindow
+app.on('ready', () => {
+    mainwindow = new BrowserWindow({})
+    //Ahora cargamos el html que se vera en Browserwindows
+    mainwindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'view/index.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+    
+    //Modificacion del menu de la ventana
+    const mainmenu = Menu.buildFromTemplate(templateMenu);
+    Menu.setApplicationMenu(mainmenu);
+
+    
+});
+
+
+const templateMenu = [
+    {
+        label: 'File', 
+        submenu: [
+            {
+                label: 'New product',
+                accelerator: 'Ctrl+N',
+                click(){
+                    alert('New product')
+                }
+            }
+        ]
+    }
+]
