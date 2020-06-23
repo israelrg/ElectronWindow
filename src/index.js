8d540e3952c1b42d6a2f5fcf92d104a7d0a4d487
Israel Rodriguez Gallego <israelrodriguez@protonmail.com>
2020-06-24 00:14:38 +0200

    Menu eliminado

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 
 //Hacer el reload solo en sistema de produccion
+//Mostrar cambios guardando
 if (process.env.NODE_ENV !== 'production'){
 require('electron-reload')(__dirname, {
 })}
@@ -11,6 +12,7 @@ require('electron-reload')(__dirname, {
 let mainwindow
 app.on('ready', () => {
     mainwindow = new BrowserWindow({})
+    
     //Ahora cargamos el html que se vera en Browserwindows
     mainwindow.loadURL(url.format({
         pathname: path.join(__dirname, 'view/index.html'),
@@ -18,14 +20,15 @@ app.on('ready', () => {
         slashes: true
     }))
     
+    mainwindow.setMenuBarVisibility(false);
     //Modificacion del menu de la ventana
-    const mainmenu = Menu.buildFromTemplate(templateMenu);
-    Menu.setApplicationMenu(mainmenu);
+    //const mainmenu = Menu.buildFromTemplate(templateMenu);
+    //Menu.setApplicationMenu(mainmenu);
 
     
 });
 
-
+/*
 const templateMenu = [
     {
         label: 'File', 
@@ -40,3 +43,4 @@ const templateMenu = [
         ]
     }
 ]
+*/
